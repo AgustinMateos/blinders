1c2855959f277ef92f9b860734df60bd0be67623
agustin mateos <agustinrotondomateos@gmail.com>
2025-10-07 01:28:19 -0300

    contacto

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 import { BigShoulders } from "../ui/fonts";
 export default function Contacto() {
     return (
-        <div className="pt-20 bg-black w-full min-h-screen text-white">
+        <div className="pt-20 px-4 md:px-4 bg-black w-full min-h-screen text-white">
             <div className=" ">
                 {/* Header Section */}
-                <div className="w-full mx-1  border-b-[1px] h-[139px] border-[#F2F2F280] flex items-center justify-start">
-                    <h1 className={`${BigShoulders} text-[#FFFFFF] font-bold text-[96px] leading-[100%] tracking-[0] uppercase`}>
+                <div className="w-full mx-1 border-t-[0.5px] border-b-[0.5px] h-[100px] md:h-[139px] border-[#222626] flex items-center justify-start">
+                    <h1 className={`${BigShoulders} text-[#FFFFFF] font-bold text-[32px] md:text-[96px] leading-[100%] tracking-[0] uppercase`}>
                         Contacto
                     </h1>
                 </div>
@@ -15,7 +15,7 @@ export default function Contacto() {
                 {/* Main Content Section */}
                 <div className="w-full flex flex-col md:flex-row justify-between py-10">
                     {/* Left Side (Image Placeholder) */}
-                    <div className="w-[50%] relative">
+                    <div className="w-full  md:w-[50%] md:relative hidden md:block" >
                         <div className="w-full h-[730px] bg-gray-800 flex items-center justify-center">
                             {/* Placeholder for the image with "BLINDERS STUDIOS" text */}
                             <div className="text-center opacity-50">
@@ -26,13 +26,13 @@ export default function Contacto() {
                     </div>
 
                     {/* Right Side (Form) */}
-                    <div className="w-[45%] pr-[30px] space-y-6">
+                    <div className="w-full md:w-[45%] md:pr-[30px] space-y-6">
                         {/* Form Fields */}
                         <div>
                             <label className="block text-sm font-medium mb-2">Nombre</label>
                             <input
                                 type="text"
-                                className="w-full p-2 bg-transparent border border-gray-400 focus:outline-none"
+                                className="w-full p-3 bg-transparent rounded-[7px] border border-gray-400 focus:outline-none"
                                 placeholder="Nombre"
                             />
                         </div>
@@ -40,7 +40,7 @@ export default function Contacto() {
                             <label className="block text-sm font-medium mb-2">Teléfono</label>
                             <input
                                 type="tel"
-                                className="w-full p-2 bg-transparent border border-gray-400 focus:outline-none"
+                                className="w-full p-3 bg-transparent rounded-[7px] border border-gray-400 focus:outline-none"
                                 placeholder="+54 9 11111111"
                             />
                         </div>
@@ -50,7 +50,7 @@ export default function Contacto() {
                             </label>
                             <input
                                 type="email"
-                                className="w-full p-2 bg-transparent border border-gray-400 focus:outline-none"
+                                className="w-full p-3 bg-transparent rounded-[7px] border border-gray-400 focus:outline-none"
                                 placeholder="nombre@ejemplo.com"
                             />
                         </div>
@@ -58,8 +58,8 @@ export default function Contacto() {
                             <label className="block text-sm font-medium mb-2">
                                 Tipo de proyecto
                             </label>
-                            <select className="w-full p-2 bg-transparent border border-gray-400 focus:outline-none">
-                                <option>¿Qué tienes en mente?</option>
+                            <select className="w-full p-3 bg-transparent rounded-[7px] border border-gray-400 focus:outline-none">
+                                <option className="text-[#5E5E5E]">¿Qué tienes en mente?</option>
                                 <option>Videoclip</option>
                                 <option>Show</option>
                                 <option>Evento</option>
@@ -72,7 +72,7 @@ export default function Contacto() {
                         <div>
                             <label className="block text-sm font-medium mb-2">Mensaje</label>
                             <textarea
-                                className="w-full p-2 bg-transparent border border-gray-400 focus:outline-none resize-none h-15"
+                                className="w-full p-3 bg-transparent rounded-[7px] border border-gray-400 focus:outline-none resize-none h-15"
                                 placeholder="Agrega un mensaje..."
                             ></textarea>
                         </div>
