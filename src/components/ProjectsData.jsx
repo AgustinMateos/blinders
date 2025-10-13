c4d6617b1bde26f6a2f20d41e5001fccdc5e8e29
agustin mateos <agustinrotondomateos@gmail.com>
2025-10-13 00:25:39 -0300

    blinders

--- src/components/ProjectsData.jsx
+++ src/components/ProjectsData.jsx
@@ -4,29 +4,34 @@ export const projects = {
       id: 'project1',
       title: ' MI CIUDAD - BHAVI x TIAGO PZK x YSY A',
       cover: '/proyectos/portadas/artistas/miCiudad.jpg',
+      tipo:'VIDEOCLIP',
       videoUrl: 'https://www.youtube.com/embed/jwC92DAq0dQ?si=4TZZa5qsTxuuEp8a', // YouTube embed URL
     },
     {
       id: 'project2',
       title: 'MIS PECADOS - BHAVI x YSY A',
       cover: '/proyectos/portadas/artistas/misPecados.jpg',
+      tipo:'VIDEOCLIP',
       videoUrl: 'https://www.youtube.com/embed/Lrd-loA_x6I?si=YmcB6rMsibLuAAsD', // Replace with actual embed URL
     },
     {
       id: 'project3',
       title: 'DREAMS&LOVE - LIT KILLAH ',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/dreams&Love.jpg',
       videoUrl: 'https://www.youtube.com/embed/mt01a7bhHMQ?si=6twuwD4Yly0dSjqa', // Replace with actual embed URL
     },
      {
       id: 'project4',
       title: 'D EL DON - KHEA x BHAVI x LIT KILLAH x ASAN ',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/elDon.jpg',
       videoUrl: 'https://www.youtube.com/embed/6RXATyGhPD8?si=qzpnKFFy-6y52XTn', // Replace with actual embed URL
     },
     {
       id: 'project5',
       title: 'CUANDO ELLA ME VIÓ - STANDLY x LIT KILLAH x KIDD ',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/cuandoEllaMeVio.jpg',
       videoUrl: 'https://www.youtube.com/embed/1oLftXI3E6M?si=IztBDJnRdp65nFan', // Replace with actual embed URL
     },
@@ -34,83 +39,97 @@ export const projects = {
       id: 'project6',
       title: 'LUNA - LUCHO SSJ x DANI RIBBA',
       cover: '/proyectos/portadas/artistas/luna.jpg',
+      tipo:'VIDEOCLIP',
       videoUrl: 'https://www.youtube.com/embed/n2YWk25GIFI?si=wr1ttc8r-xx-3Qdz', // Replace with actual embed URL
     },
     {
       id: 'project7',
       title: 'I NEED YOU - LUCHO SJJ x LIL TROCA',
       cover: '/proyectos/portadas/artistas/iNeedYou.jpg',
+      tipo:'VIDEOCLIP',
       videoUrl: 'https://www.youtube.com/embed/ikrwUWytgRc?si=28KR1sFc0qmYw356', // Replace with actual embed URL
     },
     {
       id: 'project8',
       title: 'ALMAS GEMELAS - KHEA',
       cover: '/proyectos/portadas/artistas/almas.jpg',
+      tipo:'VIDEOCLIP',
       videoUrl: 'https://www.youtube.com/embed/OGglE3O7YpU?si=39mxSY0_NjD6zatd', // Replace with actual embed URL
     },
     {
       id: 'project9',
       title: 'DEMONIA - AWP x LUCHO SSJ',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/demonia.jpg',
       videoUrl: 'https://www.youtube.com/embed/QdLcknVaeTE?si=_9V7iyMZZrrxcLKz', // Replace with actual embed URL
     },
     {
       id: 'project10',
       title: 'DICES - KHEA',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/dices.jpg',
       videoUrl: 'https://www.youtube.com/embed/iNpRLcCaypo?si=Et89iFMR9oqab-U4', // Replace with actual embed URL
     },
     {
       id: 'project11',
       title: 'OREO DE FRESA - KHEA',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/oreoDeFresa.jpg',
       videoUrl: 'https://www.youtube.com/embed/PJBkp5EE4OM?si=F6UWr68r0HdQSKiA', // Replace with actual embed URL
     },
     {
       id: 'project12',
       title: 'TOXICA - KHEA x ASAN',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/toxica.jpg',
       videoUrl: 'https://www.youtube.com/embed/vrclsLecX6s?si=5E0IDeCEmJOK1e2T', // Replace with actual embed URL
     },
      {
       id: 'project13',
       title: 'VIBEZ - KHEA',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/vibez.jpg',
       videoUrl: 'https://www.youtube.com/embed/HQCYBrC9oBE?si=fHSUWdRGc6Br3fTw', // Replace with actual embed URL
     },
      {
       id: 'project14',
       title: 'FUERA DE LA ORBITA - KHEA x YSY A',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/fueraDeLaOrbita.jpg',
       videoUrl: 'https://www.youtube.com/embed/y4m0J2dNMf4?si=TasbuJlPWMUzoP6M', // Replace with actual embed URL
     },
      {
       id: 'project15',
       title: 'ESTRELLAS EN LA NOCHE - KHEA',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/estrellasEnLaNoche.jpg',
       videoUrl: 'https://www.youtube.com/embed/jz9rxICjV4o?si=6WqfuTQCcgHJexfK', // Replace with actual embed URL
     },
      {
       id: 'project16',
       title: '23 - FALKE 912',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/falke.jpg',
       videoUrl: 'https://www.youtube.com/embed/GoCjyLeRNr0?si=tfJcn74BpnFZDBoO', // Replace with actual embed URL
     },
      {
       id: 'project17',
       title: 'MOOD - JAY OC x FALKE 912',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/mood.jpg',
       videoUrl: 'https://www.youtube.com/embed/BXsU7tl6DUg?si=bGzpF--qlOun82Od', // Replace with actual embed URL
     },
      {
       id: 'project18',
       title: 'GOT DAMN - FALKE 912',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/estrellasEnLaNoche.jpg',
       videoUrl: 'https://www.youtube.com/embed/6V_GyWVCWfU?si=aUd429XdbuhIF3Pz', // Replace with actual embed URL
     },
      {
       id: 'project19',
       title: ' PIROYANSKI - GERRE',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/piroyansky.jpg',
       videoUrl: 'https://www.youtube.com/embed/K-ehOiOEjno?si=1PdaRnlfYbuL5j5C', // Replace with actual embed URL
     },
@@ -118,54 +137,63 @@ export const projects = {
     {
       id: 'project20',
       title: 'PALPITOS - GERRE',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/palpitos.png',
       videoUrl: 'https://www.youtube.com/embed/xxxr-O0neYI?si=fDDuKnHgI2kFdfPz', // Replace with actual embed URL
     },
     {
       id: 'project21',
       title: ' TU TE FIJAS POR MI? - MAJO CHICHAR',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/piroyansky.jpg',
       videoUrl: 'https://www.youtube.com/embed/w8PXpvWr4IQ?si=RLW8YBCub4MifUQP', // Replace with actual embed URL
     },
     {
       id: 'project22',
       title: 'AP - LUCHO SSJ x SALASTKBRON x NEO PISTEA',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/ap.jpg',
       videoUrl: 'https://www.youtube.com/embed/OZs9JUaJLlA?si=ZbAQtq_LSHPOxDfJ', // Replace with actual embed URL
     },
      {
       id: 'project23',
       title: 'SALSA SUPREMA - BHAVI',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/ap.jpg',
       videoUrl: 'https://www.youtube.com/embed/UMct2YD41RU?si=M4jW9bDcViG-HkDF', // Replace with actual embed URL
     },
     {
       id: 'project24',
       title: 'CLARO QUE SI - BHAVI ',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/claroquesi.jpg',
       videoUrl: 'https://www.youtube.com/embed/bjK-UDh8OyY?si=REfECjBe0wI6htWM', // Replace with actual embed URL
     },
     {
       id: 'project25',
       title: 'AEROBICO - BHAVI',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/claroquesi.jpg',
       videoUrl: 'https://www.youtube.com/embed/A99BPQ9M5wk?si=wbQucdCTRovVFPDh', // Replace with actual embed URL
     },
     {
       id: 'project26',
       title: 'AMOR DE BARRIO - CAMPE',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/amordebarrio.jpg',
       videoUrl: 'https://www.youtube.com/embed/FQJczZnt6iA?si=U3WNjrsNWx1XW6kT', // Replace with actual embed URL
     },
     {
       id: 'project27',
       title: 'RETRO’S - CAMPEE',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/retrosthisiscampe.jpg',
       videoUrl: 'https://www.youtube.com/embed/U2brqLvIydE?si=wKHHa8mUDCF6lngU', // Replace with actual embed URL
     },
      {
       id: 'project28',
       title: 'TAMO EN ON - LUCHO SSJ x MP EL JUVENIL',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/retrosthisiscampe.jpg',
       videoUrl: 'https://www.youtube.com/embed/06r-vDW--4g?si=nnsGIRxwv_xLfQ7t', // Replace with actual embed URL
     },
@@ -173,47 +201,55 @@ export const projects = {
       id: 'project29',
       title: 'RICH BOY - MP EL JUVENIL',
       cover: '/proyectos/portadas/artistas/richboy.jpg',
+      tipo:'VIDEOCLIP',
       videoUrl: 'https://www.youtube.com/embed/TblVqCgQIo0?si=hWcpTOldbIq5ANCu', // Replace with actual embed URL
     },
      {
       id: 'project30',
       title: 'PERREITO TRISTE #1 - EICH',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste.jpg',
       videoUrl: 'https://www.youtube.com/embed/_krZmASCYnQ?si=IQ-YKg8JrKTbSHSW"', // Replace with actual embed URL
     },
      {
       id: 'project31',
       title: 'PERREITO TRISTE #2 - EICH',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste2.jpg',
       videoUrl: 'https://www.youtube.com/embed/ZbwyH7zD9HI?si=iaoPreLEQ8B7amiG', // Replace with actual embed URL
     },
      {
       id: 'project32',
       title: 'PERREITO TRISTE #3 - EICH',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://www.youtube.com/embed/opvIRrx_3P4?si=x30nFTXFEDkOTdGC', // Replace with actual embed URL
     },
      {
       id: 'project33',
       title: 'ETERNOS RETORNOS - KERSE',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://www.youtube.com/embed/YCwiXLEjBT4?si=OdaMP8OMRRzoeJba', // Replace with actual embed URL
     },
     {
       id: 'project34',
       title: 'MOVISTAR ARENA - LUCK RA',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://player.vimeo.com/video/1044072251?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479', // Replace with actual embed URL
     },
     {
       id: 'project35',
       title: 'HURACAN - YSY A',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://player.vimeo.com/video/1044067842?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479', // Replace with actual embed URL
     },
     {
       id: 'project36',
       title: 'OBRAS DIA #1 - BHAVI',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://player.vimeo.com/video/1044114452?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479', // Replace with actual embed URL
     },
@@ -226,36 +262,42 @@ export const projects = {
     {
       id: 'project38',
       title: 'MOVISTAR ARENA - KE PERSONA',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://player.vimeo.com/video/1044114708?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479', // Replace with actual embed URL
     },
     {
       id: 'project39',
       title: 'MOVISTAR ARENA - TIAGO PZK',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://player.vimeo.com/video/1085480387?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479', // Replace with actual embed URL
     },
     {
       id: 'project40',
       title: 'MOVISTAR ARENA - QUEVEDO',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://player.vimeo.com/video/1044114569?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479', // Replace with actual embed URL
     },
     {
       id: 'project41',
       title: 'MOVISTAR ARENA - ELADIO CARRION',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://player.vimeo.com/video/1123033689?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479', // Replace with actual embed URL
     },
     {
       id: 'project42',
       title: 'NICETO - ASAN',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://player.vimeo.com/video/1054259658?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479', // Replace with actual embed URL
     },
     {
       id: 'project43',
       title: 'KHEA 2024 - TOUR PRESENTACIÓN',
+      tipo:'VIDEOCLIP',
       cover: '/proyectos/portadas/artistas/perreitotriste3.jpg',
       videoUrl: 'https://player.vimeo.com/video/1123663704?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479', // Replace with actual embed URL
     },
